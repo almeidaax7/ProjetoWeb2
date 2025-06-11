c52eebe0d8e375108854334e7777e752ff2ce57b
almeidaax7 <ghab07122004@gmail.com>
2025-06-11 19:57:14 -0300

    atualizaçao header

--- estetica.php
+++ estetica.php
@@ -7,43 +7,47 @@
   <link rel="stylesheet" href="estetica.css" />
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+
 </head>
 <body>
     <header>
-  <nav class="navbar navbar-expand-lg bg-white">
-    <div class="container-fluid position-relative">
+  <nav class="navbar navbar-expand-lg bg-white shadow-sm py-3">
+    <div class="container">
 
-      
-      <a class="navbar-brand position-absolute start-50 translate-middle-x text-center" href="#">
-        Lino Estética Motors
+      <!-- Logo à esquerda -->
+      <a class="navbar-brand d-flex align-items-center" href="#">
+        <img src="img/logo.png" alt="Logo do site" class="logo me-2" />
+        <span class="nome-empresa">Lino Estética Motors</span>
       </a>
 
-      
-      <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+      <!-- Botão mobile -->
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
 
-      
+      <!-- Itens de navegação -->
       <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <div style="display: flex; justify-content: flex-end;">
-            <img class="logo" src="img/logo.png" alt="Logo Do Site">
-
-            <a class="nav-link active" href="#">Serviços</a>
+            <a class="nav-link" href="#servicos">Serviços</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link active" href="#">Sobre</a>
+            <a class="nav-link" href="#sobre">Sobre</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link active" href="#">Contato</a>
+            <a class="nav-link" href="#contato">Contato</a>
           </li>
         </ul>
       </div>
     </div>
   </nav>
 </header>
+
 
   
   <section id="servicos">
