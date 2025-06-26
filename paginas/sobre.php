4bb3f48ec52673be0fabaec7f11646cd668f53c8
almeidaax7 <ghab07122004@gmail.com>
2025-06-25 23:15:03 -0300

    efeitos no sobre

--- paginas/sobre.php
+++ paginas/sobre.php
@@ -6,6 +6,7 @@
     <title>Sobre - Lino </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+<link href="https://unpkg.com/aos@2.3.4/dist/aos.css" rel="stylesheet">
 
     <link rel="stylesheet" href="estetica.css">
 </head>
@@ -15,30 +16,29 @@
     ?>
 
     <section id="sobre" class="bg-dark text-light py-5">
-            <div class="container">
-                <h2 class="text-center text-light ">Sobre Nós</h2>
-                <div class="row align-items-center">
-                    <div class="col-md-6 text-center mb-3 mb-md-0">
-                        <div class="shadow-wrapper">
-                            <img src="../img/logo.png" alt="Equipe Lino Estética Motors" style="box-shadow: 0 0 30px rgba(155, 44, 155, 0.6);" class="img-fluid">
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <p class="lead">Somos especialistas em cuidar da sua moto como ela merece! Na Lino Estética Motors, utilizamos apenas produtos de alta qualidade e técnicas avançadas para garantir brilho, proteção e uma limpeza impecável.</p>
-                        <p>Nosso compromisso é com a satisfação do cliente e a longevidade da sua motocicleta. Confie em quem entende do assunto para deixar sua moto com aspecto de nova!</p>
-                        <a href="https://wa.me/5544998740047" target="_blank" style="display: inline-flex; align-items: center; background-color:rgb(96, 180, 127); color: white; padding: 10px 20px; border-radius: 5px; text-decoration: none; font-size: 16px;">
-    <i class="fab fa-whatsapp" style="margin-right: 10px; font-size: 20px; color: white;"></i>
-    Agende seu Horário!
-</a>
-                    </div>
-                    
-                </div>
-            </div>
-        </section>
+  <div class="container" data-aos="fade-up">
+    <h2 class="text-center text-light" data-aos="zoom-in">Sobre Nós</h2>
+    <div class="row align-items-center">
+      <div class="col-md-6 text-center mb-3 mb-md-0" data-aos="fade-right">
+        <div class="shadow-wrapper">
+          <img src="../img/logo.png" alt="Equipe Lino Estética Motors" style="box-shadow: 0 0 30px rgba(155, 44, 155, 0.6);" class="img-fluid">
+        </div>
+      </div>
+      <div class="col-md-6" data-aos="fade-left">
+        <p class="lead">Somos especialistas em cuidar da sua moto como ela merece! Na Lino Estética Motors, utilizamos apenas produtos de alta qualidade e técnicas avançadas para garantir brilho, proteção e uma limpeza impecável.</p>
+        <p>Nosso compromisso é com a satisfação do cliente e a longevidade da sua motocicleta. Confie em quem entende do assunto para deixar sua moto com aspecto de nova!</p>
+        <a href="https://wa.me/5544998740047" target="_blank" style="display: inline-flex; align-items: center; background-color:rgb(96, 180, 127); color: white; padding: 10px 20px; border-radius: 5px; text-decoration: none; font-size: 16px;" data-aos="zoom-in-up">
+          <i class="fab fa-whatsapp" style="margin-right: 10px; font-size: 20px; color: white;"></i>
+          Agende seu Horário!
+        </a>
+      </div>
+    </div>
+  </div>
+</section>
 
-    <div style="height: 50px;"></div>
+<div style="height: 50px;"></div>
 
-        <h1 class="text-center text-light">Nossa localização</h1>
+<h1 class="text-center text-light" data-aos="fade-up">Nossa localização</h1>
 <div class="d-flex justify-content-center align-items-center" style="min-height: 60vh;">
   <iframe
     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d270.8917010431268!2d-52.354333874582615!3d-24.007435836902424!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ed0b31a4fb764b%3A0x6255674636ac87c7!2sR.%20Sabi%C3%A1%2C%20320%20-%20Jardim%20Tropical%20I%2C%20Campo%20Mour%C3%A3o%20-%20PR%2C%2087310-130!5e0!3m2!1spt-BR!2sbr!4v1750522718469!5m2!1spt-BR!2sbr"
@@ -54,5 +54,10 @@
         include 'footer.php';
     ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+        <script src="https://unpkg.com/aos@2.3.4/dist/aos.js"></script>
+<script>
+  AOS.init();
+</script>
+
 </body>
 </html>
